166853bba18422cadb47db91bce71c912f96fc8c
joe <joe@joehollo.co.uk>
2021-02-06 17:51:18 +0000

    Add Client CleanupService (handles expired invocations)

---
+++ Aqueduct.Client/Cleanup/CleanupService.cs
@@ -0,0 +1,18 @@
+﻿using System.Timers;
+using Aqueduct.Shared.CallbackRegistry;
+
+namespace Aqueduct.Client.Cleanup
+{
+    public class CleanupService
+    {
+        private readonly ICallbackRegistry _callbackRegistry;
+        private readonly Timer _cleanupTimer;
+
+        public CleanupService(ICallbackRegistry callbackRegistry)
+        {
+            _callbackRegistry = callbackRegistry;
+            _cleanupTimer = new Timer(500);
+            _cleanupTimer.Elapsed += (_, _) => _callbackRegistry.ClearExpiredCallbacks();
+        }
+    }
+}
